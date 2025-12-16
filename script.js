269f756af6b7733f18badee236ae9bc82b80a05d
iqubal0612 <116081491+iqubal0612@users.noreply.github.com>
2025-12-16 10:26:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((code, index) => {
+
+    code.addEventListener("keydown", (e) => {
+
+        if (e.key >= 0 && e.key <= 9) {
+            code.value = "";
+            setTimeout(() => {
+                if (index < codes.length - 1) {
+                    codes[index + 1].focus();
+                }
+            }, 10);
+        }
+
+        else if (e.key === "Backspace") {
+            code.value = "";
+            setTimeout(() => {
+                if (index > 0) {
+                    codes[index - 1].focus();
+                }
+            }, 10);
+        }
+    });
+
+});
+

094e8205a03917e835499d77bdd945989314b527
iqubal0612 <116081491+iqubal0612@users.noreply.github.com>
2025-12-16 10:35:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-const codes = document.querySelectorAll(".code");
+const codes = document.querySelectorAll('.code');
 
 codes[0].focus();
 
 codes.forEach((code, index) => {
 
-    code.addEventListener("input", (e) => {
-        // Allow only digits
-        code.value = code.value.replace(/[^0-9]/g, "");
+    code.addEventListener('input', () => {
+        // allow only digits
+        code.value = code.value.replace(/[^0-9]/g, '');
 
         if (code.value && index < codes.length - 1) {
             codes[index + 1].focus();
         }
     });
 
-    code.addEventListener("keydown", (e) => {
-
-        if (e.key === "Backspace") {
-
-            if (code.value === "" && index > 0) {
+    code.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace') {
+            if (code.value === '' && index > 0) {
                 codes[index - 1].focus();
             }
         }
     });
+
 });

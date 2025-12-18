0ce42d7a0caea63944410f96fff9db0484fb5ba6
iqubal0612 <116081491+iqubal0612@users.noreply.github.com>
2025-12-18 15:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const codes = document.querySelectorAll(".code");
+const codes = document.querySelectorAll('.code');
 
-// Focus first input on load
 codes[0].focus();
 
 codes.forEach((input, index) => {
-    input.addEventListener("keydown", (e) => {
+    input.addEventListener('keydown', (e) => {
 
-        // Allow only digits & Backspace
-        if (e.key >= "0" && e.key <= "9") {
-            input.value = "";
+        if (e.key >= '0' && e.key <= '9') {
+            input.value = '';
             setTimeout(() => {
                 if (index < codes.length - 1) {
                     codes[index + 1].focus();
@@ -16,12 +14,9 @@ codes.forEach((input, index) => {
             }, 10);
         }
 
-        if (e.key === "Backspace") {
-            if (input.value === "" && index > 0) {
-                setTimeout(() => {
-                    codes[index - 1].focus();
-                    codes[index - 1].value = "";
-                }, 10);
+        if (e.key === 'Backspace') {
+            if (input.value === '' && index > 0) {
+                codes[index - 1].focus();
             }
         }
     });
